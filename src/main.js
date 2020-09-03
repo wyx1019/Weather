fab907e97140590339ac6ddfc4b82fc701c72f9d
WYX <yukiwyx1019@gmail.com>
2020-09-03 14:39:36 +0900

    debug

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'//追記
+import VueAxios from 'vue-axios'//追記
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios,axios)//追記
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Weather from '../views/Weather.vue' // 管理しやすいやり方
 
 Vue.use(VueRouter)
 
@@ -21,10 +22,7 @@ const routes = [
   {
     path: '/weather',
     name: 'weather',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Weather.vue')
+    component: Weather
   }
 ]
 
