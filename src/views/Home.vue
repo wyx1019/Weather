110a1569294ff658b2f45470b2acde6ea01d0a72
WYX <yukiwyx1019@gmail.com>
2020-09-07 10:02:39 +0900

    add style to weather

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,28 +11,21 @@
       <option value="Fukuoka">福岡</option>
     </select>
     <!-- <h2>今日の天気</h2> -->
-    <p class="weather_city">{{ city }}</p>
-    <p class="weather_city">{{ temp | formatTemp }}</p>
-    <p v-if="condition.main == 'Rain'">雨</p>
-    <p v-else-if="condition.main == 'Clouds'">曇り</p>
-    <p v-else-if="condition.main == 'Clear'">晴れ</p>
-    <p v-else-if="condition.main == 'Snow'">雪</p>
-    <p v-else></p>
-    <img v-show="condition.main == 'Snow'" src="../assets/snow.jpeg" alt="" />
-    <img v-show="condition.main == 'Clear'" src="../assets/clear.jpeg" alt="" />
-    <img
-      v-show="condition.main == 'Clouds'"
-      src="../assets/clouds.jpeg"
-      alt=""
-    />
-    <img v-show="condition.main == 'Rain'" src="../assets/rain.jpeg" alt="" />
-    <!-- <h2>明日の天気</h2>
-    <p class="weather_city">{{ temp }}℃</p>
-    <p v-if="condition.main == 'Rain'">雨</p>
-    <p v-else-if="condition.main == 'Clouds'">曇り</p>
-    <p v-else-if="condition.main == 'Clear'">晴れ</p>
-    <p v-else-if="condition.main == 'Snow'">雪</p>
-    <p v-else></p> -->
+    <h2 class="weather_city">{{ city }}</h2>
+    <div class="weather_temp">{{ temp | formatTemp }}</div>
+    <div class="weather_condition" v-if="condition.main == 'Rain'">雨</div>
+    <div class="weather_condition" v-else-if="condition.main == 'Clouds'">
+      曇り
+    </div>
+    <div class="weather_condition" v-else-if="condition.main == 'Clear'">
+      晴れ
+    </div>
+    <div class="weather_condition" v-else-if="condition.main == 'Snow'">雪</div>
+    <div v-else></div>
+    <img v-show="condition.main == 'Snow'" src="../assets/snow.jpeg" alt />
+    <img v-show="condition.main == 'Clear'" src="../assets/clear.jpeg" alt />
+    <img v-show="condition.main == 'Clouds'" src="../assets/clouds.jpeg" alt />
+    <img v-show="condition.main == 'Rain'" src="../assets/rain.jpeg" alt />
   </div>
 </template>
 
@@ -61,14 +54,17 @@ export default {
           .get(apiURL)
           .then(
             //axiosの処理が成功した場合に処理させたいことを記述
-            function(response) {
+            function (response) {
               this.city = response.data.city.name;
               this.temp = response.data.list[0].main.temp;
               this.condition = response.data.list[0].weather[0];
               // console.log(response);
             }.bind(this)
+            //this.axiosの続きにあるfunctionなので、thisの内容が変わる。
+            //functionの中にまたthisを使いたいなら、.bind(this)をつけないとダメ
+            //もう一つの解決策として、コールバックをarrow functionで書くこともできる
           )
-          .catch(function(error) {
+          .catch(function (error) {
             //axiosの処理にエラーが発生した場合に処理させたいことを記述
             console.log(error);
           });
@@ -77,9 +73,31 @@ export default {
     },
   },
   filters: {
-    formatTemp: function(val) {
+    formatTemp: function (val) {
       return (val = val + "℃");
     },
   },
 };
 </script>
+
+<style scoped>
+.weather_city {
+  font-weight: bold;
+}
+.weather_temp {
+  font-size: 18px;
+}
+div {
+  margin: 10px;
+  font-weight: bold;
+}
+h2 {
+  margin-bottom: 0;
+}
+select {
+  padding: 5px 10px 7px;
+  width: 20%;
+  max-width: 150px;
+  text-align: center;
+}
+</style>
