4a32d698d9ac1cec7be147a5f0023531cabe1e1d
WYX <yukiwyx1019@gmail.com>
2020-10-07 23:11:47 +0900

    heroku setup

---
+++ dist/js/chunk-2d22d746.8f8b18f4.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},u=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"user-list"},[n("h1",[t._v("ユーザーリスト")]),n("div",[n("input",{staticClass:"btn",attrs:{type:"button",value:""}})])])}],a=n("2877"),l={},c=Object(a["a"])(l,s,u,!1,null,null,null);e["default"]=c.exports}}]);
+//# sourceMappingURL=chunk-2d22d746.8f8b18f4.js.map
